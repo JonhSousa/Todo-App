1325ce22c69d0faff5c0b2e6a290616910eedf26
JonhSousa <jonhnbsousa@gmail.com>
2016-01-25 00:40:37 -0200

    Some modifications in test Files
    Create and consume test files were changed to adapt the script.

--- cliente-android.js
+++ cliente-android.js
@@ -3,7 +3,7 @@ var http = require('http');
 var configuracoes = {
     hostname : 'localhost',
     port : 3000,
-    path : '/produtos',
+    path : '/atividades',
     headers: {
         'Accept':'application/json'
     }
